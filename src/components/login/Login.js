fbadd65e094b335c5d9af0ed9540695a48a7fe93
srikrishnas <srikrisna1234@gmail.com>
2021-04-03 10:31:32 +0530

    image updates

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,50 +1,62 @@
-import React,{useState,useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import { WrapperContext } from '../../App';
 import { login } from '../interactions'
 
-const Login = ({handleLogin,history}) => {
+import loginImage from '../../media/login.png'
+
+const Login = ({ handleLogin, history }) => {
 
     const { logout } = useContext(WrapperContext);
 
-    const [userdata,setUser]=useState({email:"",password:""})
+    const [userdata, setUser] = useState({ email: "", password: "" })
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(userdata)
-        const {email,password} = userdata
-        login(email,password)
-        .then((data) => {
-            console.log("Login data :::",data)
-            const {name,token} = data;
-            handleLogin(name,token)
-            if(name){
-                history.push(`/mainpage`)
-            } else {
-                history.push(`/login`)
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-        
+        const { email, password } = userdata
+        login(email, password)
+            .then((data) => {
+                console.log("Login data :::", data)
+                const { name, token } = data;
+                handleLogin(name, token)
+                if (name) {
+                    history.push(`/mainpage`)
+                } else {
+                    history.push(`/login`)
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
     }
 
     return (
         <div className="container">
-            <form className="card border-dark" style={{width: '20rem'}}  onSubmit={handleSubmit}>
-                <div className="card-body">
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={userdata.email} onChange={(e)=>{setUser({...userdata, email:e.target.value})}} />
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" value={userdata.password} onChange={(e)=>{setUser({...userdata, password:e.target.value})}}/>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+            <div className="row m-5">
+
+                <div className="col-6">
+                    <img src={loginImage} width="400" height="400" className="d-inline-block align-top" alt="" />
                 </div>
-            </form>
+
+                <div className="col-6">
+                    <form className="card border-dark" style={{ width: '20rem' }} onSubmit={handleSubmit} autoComplete="off">
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={userdata.email} onChange={(e) => { setUser({ ...userdata, email: e.target.value }) }} />
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" value={userdata.password} onChange={(e) => { setUser({ ...userdata, password: e.target.value }) }} />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,44 +1,52 @@
 import React, { useState } from 'react'
 import { register } from '../interactions'
+import loginImage from '../../media/login.png'
 
-const Register = ({history}) => {
+const Register = ({ history }) => {
 
-    const [user, setUser] = useState({ email: "", password: "" , name: ""})
+    const [user, setUser] = useState({ email: "", password: "", name: "" })
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(user)
-        const {email, password, name} = user
+        const { email, password, name } = user
         register(email, password, name)
-        .then((data) => {
-            console.log(data)
-            if(data.message){
-                history.push(`/login`)
-            }
-        })
+            .then((data) => {
+                console.log(data)
+                if (data.message) {
+                    history.push(`/login`)
+                }
+            })
     }
 
     return (
         <div className="container">
-            <form className="card border-dark" style={{ width: '40rem' }} onSubmit={handleSubmit}>
-                <div className="card-body">
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label htmlFor="inputEmail4">Email</label>
-                            <input type="email" className="form-control" id="inputEmail4" placeholder="Email" value={user.email} onChange={(e) => setUser({...user, email:e.target.value})}/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="inputPassword4">Password</label>
-                            <input type="password" className="form-control" id="inputPassword4" placeholder="Password" value={user.password} onChange={(e) => setUser({...user, password:e.target.value})}/>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputAddress">Name</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="Full Name" value={user.name} onChange={(e) => setUser({...user, name:e.target.value})}/>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Register!</button>
+            <div className="row mt-5">
+                <div className="col-6">
+                    <img src={loginImage} width="400" height="400" className="d-inline-block align-top" alt="" />
                 </div>
-            </form>
+                <div className="col-6">
+                    <form className="card border-dark" style={{ width: '30rem' }} onSubmit={handleSubmit}  autocomplete="off">
+                        <div className="card-body">
+                            <div className="form-row">
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputEmail4">Email</label>
+                                    <input type="email" className="form-control" id="inputEmail4" placeholder="Email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputPassword4">Password</label>
+                                    <input type="password" className="form-control" id="inputPassword4" placeholder="Password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="inputAddress">Name</label>
+                                <input type="text" className="form-control" id="inputAddress" placeholder="Full Name" value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })} />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Register!</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
